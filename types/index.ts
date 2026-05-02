af0a0bed5991f891ff9bd3b70fea1f5871bb32a6
Rustam <krr12@mail.ru>
2026-05-02 17:35:06 +0500

    Add types/index.ts

--- types/index.ts
+++ types/index.ts
@@ -5,21 +5,10 @@ export interface User {
   phone?: string;
   phone_verified_at?: string;
   is_admin?: boolean;
-  avatar?: string;            // относительный или полный путь
-  avatar_url?: string;        // полный URL (вычисляется на бэке)
+  avatar?: string;
+  avatar_url?: string;
   created_at: string;
   updated_at: string;
-}
-export interface City {
-  id: number;
-  name: string;
-  pivot?: {
-    price_per_day: string;
-    buyout_price: string | null;
-    description: string | null;
-    is_available: boolean;
-     cities?: City[];  // <-- добавить
-  };
 }
 
 export interface Car {
@@ -36,7 +25,7 @@ export interface Car {
   is_available: boolean;
   created_at: string;
   updated_at: string;
-  user?: Pick<User, 'id' | 'name' | 'phone' | 'avatar_url'>; // добавлено avatar_url
+  user?: Pick<User, 'id' | 'name' | 'phone' | 'avatar_url'>;
   views_today?: number;
   views_count?: number;
 }
